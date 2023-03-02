8c6a1d7468ea70a0a268b2389122d7048ae7800f
michele <michele.giovagnoli1@gmail.com>
2023-03-02 17:50:42 +0100

    Pari e dispari quasi finito

--- paridispari/main.js
+++ paridispari/main.js
@@ -1,3 +1,41 @@
+const sceltaUtente = prompt('scegli se pari o dispari');
+const numeroUtente = parseInt(prompt('scegli un numero da 1 a 5'));
+
+console.log(numeroUtente);
+
+
+const somma = numeroUtente + numeroRandom(1,5);
+console.log(somma);
+const risultatoPariDispari = pariDispari(somma);
+
+
+if (sceltaUtente == risultatoPariDispari) {
+    console.log('complimenti hai vinto');
+}
+else {
+    console.log('ritenta e sarai piu fortunato');
+}
+
+
+
+
+
+function pariDispari (valore) {
+    
+    let risultato = '';
+    
+    if (valore % 2 == 0) { 
+        risultato = 'pari';
+    }
+    else {
+        risultato = 'dispari';
+    }
+    return risultato;
+}
+
+
 function numeroRandom (min, max) {
-    Math.floor(math.random()* max) + min;
+  const numeroRandom = Math.floor( Math.random() * (max - min + 1) ) + min;
+  console.log(numeroRandom);
+  return numeroRandom;
 }
